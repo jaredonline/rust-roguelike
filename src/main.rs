ce6b9b7d5c4c6b1d0a56f1df75f709ed0608ff25
Jared McFarland <jared.online@gmail.com>
2014-08-12 19:27:32 -0400

    Organizing our code into a game loop; creating the render function

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 extern crate tcod;
 use tcod::{Console, background_flag, key_code, Special};
 
+fn render(con: &mut Console) {
+    con.clear();
+    con.put_char(40, 25, '@', background_flag::Set);
+    con.flush();
+}
+
 fn main() {
     let mut con = Console::init_root(80, 50, "libtcod Rust tutorial", false);
     let mut exit = false;
+    // render
+    render(&mut con);
     while !(Console::window_closed() || exit) {
-        con.clear();
-        con.put_char(40, 25, '@', background_flag::Set);
-        con.flush();
+        // wait for user input
         let keypress = con.wait_for_keypress(true);
+
+        // update game state
         match keypress.key {
             Special(key_code::Escape) => exit = true,
             _ => {}
         }
+
+        // render
+        render(&mut con);
     }
 }
